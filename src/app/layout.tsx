4d3405a8f42a8b97dc38af078db1ddc1920b545d
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2025-10-14 01:03:00 +0530

    refactor+feat(fonts, theme): google fonts and colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,8 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import '@fontsource-variable/google-sans-code';
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-const googleSans = Roboto({
-  variable: "--font-google-sans",
-  subsets: ["latin"],
-  weight: ["400", "500", "700"]
-});
 
 export const metadata: Metadata = {
   title: "GDG NMIT",
@@ -24,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${googleSans.variable} antialiased`}
+        className={`font-sans antialiased`}
       >
         <Header/>
         {children}
