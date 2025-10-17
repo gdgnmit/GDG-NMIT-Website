dd8a6bc4666b8f8127214080b37813bc5b807b40
Krishna Shrivastav <147995983+krishna-shrivastav232702@users.noreply.github.com>
2025-10-18 02:23:03 +0530

    Add Domain and Tier enums to Team model

--- src/lib/models/Team.ts
+++ src/lib/models/Team.ts
@@ -1,9 +1,33 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+export enum Domain {
+  CLUB = 'club',
+  TECH = 'tech',
+  DESIGN = 'design',
+  CONTENT_AND_DOCUMENTATION = 'content and documentation',
+  PR_AND_MARKETING = 'pr & marketing',
+  SOCIAL_MEDIA = 'social media',
+  OPERATIONS = 'operations'
+}
+
+export enum Tier {
+  FACULTY_ADVISOR = 'faculty advisor',
+  LEAD = 'lead',
+  CO_LEAD = 'co-lead',
+  STUDENT_ADVISOR = 'student-advisor',
+  MENTOR = 'mentor',
+  CORE = 'core',
+  MEMBER = 'member',
+  PAST = 'past',
+  RECRUIT = 'recruit'
+}
+
 export interface IMember {
-  teamId:string,
+  teamId: string;
   name: string;
   role: string;
+  domain: Domain;
+  tier: Tier;
   photoUrl: string;
 }
 
@@ -13,17 +37,27 @@ export interface ITeam extends Document {
   members: IMember[];
 }
 
-const MemberSchema: Schema = new Schema<IMember>({
+const MemberSchema = new Schema<IMember>({
   teamId: { type: String, required: true },
   name: { type: String, required: true },
   role: { type: String, required: true },
+  domain: { 
+    type: String,
+    required: true,
+    enum: Object.values(Domain)
+  },
+  tier: { 
+    type: String,
+    required: true,
+    enum: Object.values(Tier)
+  },
   photoUrl: { type: String }
 });
 
-const TeamSchema: Schema = new Schema<ITeam>({
+const TeamSchema = new Schema<ITeam>({
   year: { type: Number, required: true, index: true },
-  name: { type:String},
-  members: { type: [MemberSchema], default: [] }
+  name: { type: String },
+  members: { type: [MemberSchema], default: [] },
 });
 
 export default mongoose.models.Team || mongoose.model<ITeam>('Team', TeamSchema);
