3a9f84eeb7df71bf4f6b06e0e65985d231515c2e
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2025-10-19 00:39:15 +0530

    chore: project page template

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <main className="py-16 min-h-screen bg-white dark:bg-g-almost-black">
+      <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-12 py-12 lg:py-16">
+        <div className="mb-12">
+          <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-900 dark:text-white">
+            Projects
+          </h2>
+          <div className="h-1 w-20 bg-gradient-to-r from-g-blue via-g-red to-g-yellow rounded-full mt-2"></div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Projects
+export default Projects;
